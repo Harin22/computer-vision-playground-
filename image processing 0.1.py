569e8a1c4bd050f3f68515bc3cf60de9f8af62ab
Harin <84919488+Harin22@users.noreply.github.com>
2025-02-27 02:36:59 +0530

    image processing in an image
    this include resizing, scaling, color conversion, and displaying images with OpenCV.

---
+++ image processing 0.1.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+import numpy as np
+
+#Read the original image
+image = cv.imread("D:\K.jpg")
+
+
+image_info = image.shape
+print(image_info[0])
+
+resizing_img = cv.resize(image,(300, 300))
+cv.imshow("Resized Image", resizing_img)
+
+scale_percent = 50  
+width = int(image.shape[1] * scale_percent / 100)
+height = int(image.shape[0] * scale_percent / 100)
+resized = cv.resize(image, (width, height))
+cv.imshow("Resized image", resized)
+
+#converting grayscale image
+hsv = cv.cvtColor(resized, cv.COLOR_BGR2HSV)
+lab = cv.cvtColor(resized, cv.COLOR_BGR2LAB)
+cv.imshow("HSV", hsv)
+cv.imshow("LAB", lab)
+
+
+
+
+#Wait for a key press and close the windows
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+
