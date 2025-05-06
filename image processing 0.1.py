8372d8ebd3e184ae6e7ff3a816401b98b66538f0
Harin <84919488+Harin22@users.noreply.github.com>
2025-05-06 22:56:56 +0530

    Update image processing 0.1.py

--- image processing 0.1.py
+++ image processing 0.1.py
@@ -11,6 +11,7 @@ print(image_info[0])
 resizing_img = cv.resize(image,(300, 300))
 cv.imshow("Resized Image", resizing_img)
 
+#resizingimage
 scale_percent = 50  
 width = int(image.shape[1] * scale_percent / 100)
 height = int(image.shape[0] * scale_percent / 100)
